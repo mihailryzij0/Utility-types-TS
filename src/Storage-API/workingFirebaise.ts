2cdc7efe58e58e44919f5f87e783fa7e161faa3b
mihailryzij0 <mihailryzij0@gmail.com>
2022-02-09 16:53:46 +0300

    fix code style

--- src/Storage-API/workingFirebaise.ts
+++ src/Storage-API/workingFirebaise.ts
@@ -1,6 +1,5 @@
-
 import { ref, set, get, child } from "firebase/database";
-import { db, dbRef} from "./firebaseConfig";
+import { db, dbRef } from "./firebaseConfig";
 import { IworkingStorage, Task } from "./intrfisceProgram";
 
 export class WorkingFirebaise implements IworkingStorage {
