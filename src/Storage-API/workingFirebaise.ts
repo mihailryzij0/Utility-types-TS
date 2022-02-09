4dcff282fed64beb295f8dc3a8a7b9267abcec2d
mihailryzij0 <mihailryzij0@gmail.com>
2022-02-09 16:51:43 +0300

    fix firebaise

--- src/Storage-API/workingFirebaise.ts
+++ src/Storage-API/workingFirebaise.ts
@@ -1,10 +1,11 @@
-import { ref, set, get } from "firebase/database";
-import { db, dbRef } from "./firebaseConfig";
+
+import { ref, set, get, child } from "firebase/database";
+import { db, dbRef} from "./firebaseConfig";
 import { IworkingStorage, Task } from "./intrfisceProgram";
 
 export class WorkingFirebaise implements IworkingStorage {
   async loadStorageArray(): Promise<any> {
-    const res = await get(dbRef, `users/`);
+    const res = await get(dbRef);
     const data = await res.val();
     return data.users;
   }
